ce227dafe26c72b8b25cf3ccada92e46d9515829
anselmo <anselmo.vecchio.jose@gmail.com>
2025-01-20 20:00:27 -0300

    New projetc Chatbot

--- src/views/MyProject/project.jsx
+++ src/views/MyProject/project.jsx
@@ -3,6 +3,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import swagger from "../.././assets/SWAGGER.png"
 import cybergames from "../.././assets/cybergames.png"
+import chatbot from "../.././assets/chatbot.png"
 import './projetc.css'
 
 function Project() {
@@ -12,6 +13,12 @@ function Project() {
     fontSize: '60px'}}> Proyectos</h2>
       
       <div className="d-flex justify-content-center flex-wrap cardContainer">
+      <div className="card m-5" style={{ width: '45rem', height: "100%", border:'3px solid green', background:'black' }}>
+          <h5 className="card-title" style={{textAlign:'center', margin:'2%', color:'white', fontSize:'2rem'}}>Chatbot</h5>
+          <div className="card-body cardBody">
+            <a href="https://chatbot-de-sushi.vercel.app/"> <img src={chatbot} className="card-img-top imgProjetc" alt="..." style={{height:'100%'}}/> </a>
+          </div>
+        </div>
         <div className="card m-5" style={{ width: '45rem', height: "100%", border:'3px solid green', background:'black' }}>
           <h5 className="card-title" style={{textAlign:'center', margin:'2%', color:'white', fontSize:'2rem'}}>CyberGames</h5>
           <div className="card-body cardBody">
