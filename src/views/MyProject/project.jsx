b6fc08ed7d9befe119422c29ce424abe52142bc8
anselmo <anselmo.vecchio.jose@gmail.com>
2026-03-17 17:23:19 -0300

    Update structure

--- src/views/MyProject/project.jsx
+++ src/views/MyProject/project.jsx
@@ -1,342 +1,68 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import swagger from "../.././assets/SWAGGER.png";
-// import cybergames from "../.././assets/cybergames.png"
-import cdlRecruiter from "../.././assets/cdlrecruiterPorfolio.png";
-import ecommerce from "../.././assets/ecommerce.png"
-import chatbot from "../.././assets/chatbot.png";
-import calculator from "../.././assets/calculator.png";
-import movies from "../.././assets/pelis.png";
-import "./projetc.css";
-
 import { useState } from "react";
+import projects from "./projects.js";
+import "./Project.css";
+
+// imágenes
+import cdlRecruiter from "../../assets/cdlrecruiterPorfolio.png";
+import ecommerce from "../../assets/ecommerce.png";
+import chatbot from "../../assets/chatbot.png";
+import movies from "../../assets/pelis.png";
+import calculator from "../../assets/calculator.png";
+import swagger from "../../assets/SWAGGER.png";
+
+const images = {
+  cdlRecruiter,
+  ecommerce,
+  chatbot,
+  movies,
+  calculator,
+  swagger
+};
 
 function Project() {
-  const [mostrarInfo, setMostrarInfo] = useState(false);
+  const [openCard, setOpenCard] = useState(null);
+
+  const toggleCard = (id) => {
+    setOpenCard(openCard === id ? null : id);
+  };
+
   return (
     <div id="project" className="project">
-      <h2
-        className="text-center"
-        style={{ margin: " 7% 0 3% 0", fontSize: "60px" }}
-      >
-        {" "}
-        Mis proyectos
-      </h2>
+      <h2 className="projectTitle">Mis proyectos</h2>
 
-      <div className="d-flex justify-content-center flex-wrap cardContainer">
-        <div
-          className="card m-5"
-          style={{
-            width: "45rem",
-            height: "100%",
-            border: "3px solid green",
-            borderRadius: "3%",
-            background: "black",
-          }}
-        >
-          <h5
-            className="card-title"
-            style={{
-              textAlign: "center",
-              margin: "2%",
-              marginBottom: "%",
-              color: "white",
-              fontSize: "2rem",
-            }}
-          >
-            CDL Recruiter
-          </h5>
-          <div className="card-body cardBody">
-            <a href="https://www.cdl-recruiters.com">
-              {" "}
-              <img
-                src={cdlRecruiter}
-                className="card-img-top imgProjetc"
-                alt="cdlRecruiter"
-                style={{ height: "100%" }}
-              />{" "}
-            </a>
-          </div>
-          <div className="InfoMore">
-            <span className="InfoMoreTittle">Mas informacion</span>
-            <button className="InfoMoreButton"
-              onClick={() => setMostrarInfo(!mostrarInfo)}
-            >
-              {mostrarInfo ? "-" : "+"}
-            </button>
-          </div>
-          {mostrarInfo && (
-            <div className="InfoMoreDescription">
-              <p>
-                CDL Recruiter es una plataforma web para optimizar el reclutamiento 
-                de conductores con licencia comercial (CDL). Se enfoca en mejorar la experiencia de usuario y 
-                automatizar el proceso de selección para empresas de transporte.
-              </p>
+      <div className="cardContainer">
+        {projects.map((project) => (
+          <div key={project.id} className="card">
+            <h5 className="cardTitle">{project.title}</h5>
+
+            <div className="cardBody">
+              <a href={project.link}>
+                <img
+                  src={images[project.img]}
+                  className="imgProject"
+                  alt={project.title}
+                />
+              </a>
             </div>
-          )}
-        </div>
 
-        <div
-          className="card m-5"
-          style={{
-            width: "45rem",
-            height: "100%",
-            border: "3px solid green",
-            borderRadius: "3%",
-            background: "black",
-          }}
-        >
-          <h5
-            className="card-title"
-            style={{
-              textAlign: "center",
-              margin: "2%",
-              color: "white",
-              fontSize: "2rem",
-            }}
-          >
-            E-commerce
-          </h5>
-          <div className="card-body cardBody">
-            <a href="https://store-jade-five.vercel.app">
-              {" "}
-              <img
-                src={ecommerce}
-                className="card-img-top imgProjetc"
-                alt="ecommerce"
-                style={{ height: "100%" }}
-              />{" "}
-            </a>
-          </div>
-          <div className="InfoMore">
-            <span className="InfoMoreTittle">Mas informacion</span>
-            <button className="InfoMoreButton"
-              onClick={() => setMostrarInfo(!mostrarInfo)}
-            >
-              {mostrarInfo ? "-" : "+"}
-            </button>
-          </div>
-          {mostrarInfo && (
-            <div className="InfoMoreDescription">
-              <p>
-                Free Market – Compra fácil, rápida y sin complicaciones <br /> <br />
+            <div className="InfoMore">
+              <span className="InfoMoreTittle">Mas informacion</span>
 
-Free Market es una aplicación web de comercio electrónico diseñada para ofrecer una experiencia de compra simple, rápida y atractiva. Los usuarios pueden explorar productos, buscarlos fácilmente y agregarlos a su carrito en tiempo real, sin necesidad de registrarse o iniciar sesión. <br /> <br />
-
-Con una interfaz moderna y una navegación fluida, la plataforma permite descubrir productos y gestionar el carrito de forma dinámica, brindando una experiencia cómoda y accesible para cualquier visitante. <br /> <br />
- 
-Explora, descubre y arma tu carrito en segundos.
-              </p>
+              <button
+                className="InfoMoreButton"
+                onClick={() => toggleCard(project.id)}
+              >
+                {openCard === project.id ? "-" : "+"}
+              </button>
             </div>
-          )}
-        </div>
 
-        <div
-          className="card m-5"
-          style={{
-            width: "45rem",
-            height: "100%",
-            border: "3px solid green",
-            borderRadius: "3%",
-            background: "black",
-          }}
-        >
-          <h5
-            className="card-title"
-            style={{
-              textAlign: "center",
-              margin: "2%",
-              color: "white",
-              fontSize: "2rem",
-            }}
-          >
-            Chatbot
-          </h5>
-          <div className="card-body cardBody">
-            <a href="https://chatbot-de-sushi-anselmos-projects-96233391.vercel.app/">
-              {" "}
-              <img
-                src={chatbot}
-                className="card-img-top imgProjetc"
-                alt="chatbot"
-                style={{ height: "100%" }}
-              />{" "}
-            </a>
+            {openCard === project.id && (
+              <div className="InfoMoreDescription">
+                <p>{project.description}</p>
+              </div>
+            )}
           </div>
-          <div className="InfoMore">
-            <span className="InfoMoreTittle">Mas informacion</span>
-            <button className="InfoMoreButton"
-              onClick={() => setMostrarInfo(!mostrarInfo)}
-            >
-              {mostrarInfo ? "-" : "+"}
-            </button>
-          </div>
-          {mostrarInfo && (
-            <div className="InfoMoreDescription">
-              <p>
-                Este es un proyecto de chatbot interactivo para un restaurante de sushi, donde los usuarios pueden consultar el menú, realizar pedidos y obtener información sobre el restaurante de forma automática.
-              </p>
-            </div>
-          )}
-        </div>
-
-        <div
-          className="card m-5"
-          style={{
-            width: "45rem",
-            height: "100%",
-            border: "3px solid green",
-            borderRadius: "3%",
-            background: "black",
-          }}
-        >
-          <h5
-            className="card-title"
-            style={{
-              textAlign: "center",
-              margin: "2%",
-              color: "white",
-              fontSize: "2rem",
-            }}
-          >
-            Peliculas
-          </h5>
-          <div className="card-body cardBody">
-            <a href="https://cine-atuxthdcx-anselmos-projects-96233391.vercel.app">
-              {" "}
-              <img
-                src={movies}
-                className="card-img-top imgProjetc"
-                alt="movies"
-                style={{ height: "22rem" }}
-              />{" "}
-            </a>
-          </div>
-          <div className="InfoMore">
-            <span className="InfoMoreTittle">Mas informacion</span>
-            <button className="InfoMoreButton"
-              onClick={() => setMostrarInfo(!mostrarInfo)}
-            >
-              {mostrarInfo ? "-" : "+"}
-            </button>
-          </div>
-          {mostrarInfo && (
-            <div className="InfoMoreDescription">
-              <p>
-                Este proyecto es una aplicación web desarrollada en JavaScript que permite explorar un catálogo de películas de manera organizada e intuitiva. La plataforma ofrece información detallada de cada título, incluyendo: <br /> <br />
-
-⭐ Valoración <br />
-
-📝 Descripción <br />
-
-⏱️ Duración <br />
-
-🎬 Director <br />
-
-🎭 Género <br /> <br />
-
-Además, cuenta con una sección dedicada a las películas próximas a estrenarse, lo que brinda una experiencia completa para quienes buscan mantenerse informados sobre novedades del cine.
-              </p>
-            </div>
-          )}
-        </div>
-
-        <div
-          className="card m-5"
-          style={{
-            width: "45rem",
-            height: "100%",
-            border: "3px solid green",
-            borderRadius: "3%",
-            background: "black",
-          }}
-        >
-          <h5
-            className="card-title"
-            style={{
-              textAlign: "center",
-              margin: "2%",
-              color: "white",
-              fontSize: "2rem",
-            }}
-          >
-            Calculadora
-          </h5>
-          <div className="card-body cardBody">
-            <a href="https://calculator-five-indol.vercel.app/calculator">
-              {" "}
-              <img
-                src={calculator}
-                className="card-img-top imgProjetc"
-                alt="calculator"
-                style={{ height: "22rem" }}
-              />{" "}
-            </a>
-          </div>
-          <div className="InfoMore">
-            <span className="InfoMoreTittle">Mas informacion</span>
-            <button className="InfoMoreButton"
-              onClick={() => setMostrarInfo(!mostrarInfo)}
-            >
-              {mostrarInfo ? "-" : "+"}
-            </button>
-          </div>
-          {mostrarInfo && (
-            <div className="InfoMoreDescription">
-              <p>
-                Esta calculadora te permite realizar operaciones simples como sumar, restar, dividir y multiplicar.
-              </p>
-            </div>
-          )}
-        </div>
-
-        <div
-          className="card m-5"
-          style={{
-            width: "45rem",
-            height: "100%",
-            border: "3px solid green",
-            borderRadius: "3%",
-            background: "black",
-          }}
-        >
-          <h5
-            className="card-title"
-            style={{
-              textAlign: "center",
-              margin: "2%",
-              color: "white",
-              fontSize: "2rem",
-            }}
-          >
-            Api rest
-          </h5>
-          <div className="card-body cardBody">
-            <a href="https://ecommerce-00vc.onrender.com/API">
-              {" "}
-              <img
-                src={swagger}
-                className="card-img-top imgProjetc"
-                alt="swagger"
-                style={{ height: "100%" }}
-              />{" "}
-            </a>
-          </div>
-          <div className="InfoMore">
-            <span className="InfoMoreTittle">Mas informacion</span>
-            <button className="InfoMoreButton"
-              onClick={() => setMostrarInfo(!mostrarInfo)}
-            >
-              {mostrarInfo ? "-" : "+"}
-            </button>
-          </div>
-          {mostrarInfo && (
-            <div className="InfoMoreDescription">
-              <p>
-                Este e-commerce de dispositivos electrónicos, desarrollado en backend con NestJS y PostgreSQL, está diseñado para permitir a los usuarios visualizar y adquirir productos, como celulares, tablets y computadoras, mediante la creación de una cuenta de usuario.
-              </p>
-            </div>
-          )}
-        </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/views/MyProject/projects.js
@@ -0,0 +1,49 @@
+const projects = [
+    {
+        id: 1,
+        title: "CDL Recruiter",
+        link: "https://www.cdl-recruiters.com",
+        img: "cdlRecruiter",
+        description:
+            "CDL Recruiter es una plataforma web para optimizar el reclutamiento de conductores con licencia comercial (CDL). Se enfoca en mejorar la experiencia de usuario y automatizar el proceso de selección para empresas de transporte.",
+    },
+    {
+        id: 2,
+        title: "E-commerce",
+        link: "https://store-jade-five.vercel.app",
+        img: "ecommerce",
+        description:
+            "Free Market Compra fácil, rápida y sin complicaciones Free Market es una aplicación web de comercio electrónico diseñada para ofrecer una experiencia de compra simple, rápida y atractiva. Los usuarios pueden explorar productos, buscarlos fácilmente y agregarlos a su carrito en tiempo real, sin necesidad de registrarse o iniciar sesión. Con una interfaz moderna y una navegación fluida, la plataforma permite descubrir productos y gestionar el carrito de forma dinámica, brindando una experiencia cómoda y accesible para cualquier visitante. Explora, descubre y arma tu carrito en segundos.",
+    },
+    {
+        id: 3,
+        title: "Chatbot",
+        link: "https://chatbot-de-sushi-anselmos-projects-96233391.vercel.app/",
+        img: "chatbot",
+        description:
+        "Este es un proyecto de chatbot interactivo para un restaurante de sushi, donde los usuarios pueden consultar el menú, realizar pedidos y obtener información sobre el restaurante de forma automática.",
+    },
+    {
+        id: 4,
+        title: "Movies",
+        link: "https://cine-atuxthdcx-anselmos-projects-96233391.vercel.app",
+        img: "movies",
+        description:"Este proyecto es una aplicación web desarrollada en JavaScript que permite explorar un catálogo de películas de manera organizada e intuitiva. La plataforma ofrece información detallada de cada título, incluyendo:⭐ Valoración📝 Descripción⏱️ Duración🎬 Director🎭 Género. Además, cuenta con una sección dedicada a las películas próximas a estrenarse, lo que brinda una experiencia completa para quienes buscan mantenerse informados sobre novedades del cine.",
+    },
+    {
+        id: 5,
+        title: "Calculadora",
+        link: "https://calculator-five-indol.vercel.app/calculator",
+        img: "calculator",
+        description: "Esta calculadora te permite realizar operaciones simples como sumar, restar, dividir y multiplicar.",
+    },
+    {
+        id: 6,
+        title: "Api Rest",
+        link:"https://ecommerce-00vc.onrender.com/API",
+        img:"swagger",
+        description: "Este e-commerce de dispositivos electrónicos, desarrollado en backend con NestJS y PostgreSQL, está diseñado para permitir a los usuarios visualizar y adquirir productos, como celulares, tablets y computadoras, mediante la creación de una cuenta de usuario.",
+    },
+];
+
+export default projects;
